916d12307de6075d2710216d91ed89d2c2ec6da0
Your Name <harshit.hartron@gmail.com>
2018-03-10 15:20:00 +0530

    properties added

--- src/app/components/Home.component.js
+++ src/app/components/Home.component.js
@@ -2,9 +2,19 @@ import React from "react";
 
 export class HomeComponent extends React.Component{
     render(){
+        let vm = this.props;
         return(
             <div>
                 <p>In a New COmponent!!</p>
+                <p>Your Name is : {vm.name} {vm.lastname} </p>
+                <p>You live in {vm.address} </p>
+                <p> User Object => Name: {vm.user.name} </p>
+                <div>
+                    <h4> Hobbies </h4>
+                    <ul>
+                        {vm.user.hobbies.map((hobby) => <li> {hobby} </li>)}
+                    </ul>
+                </div>
             </div>
         );
     }
--- src/app/index.js
+++ src/app/index.js
@@ -6,25 +6,23 @@ import { HomeComponent } from  "./components/Home.component";
 
 class App extends React.Component {
     render() {
-        let component="ghas";
-        if (true){
-            component=<p>Sass</p>;
-        }
+        let user = {
+            name:'Anna',
+            hobbies:['Sports','Car Racing']
+        };
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
                         <HeaderComponent/>
-                        { component }
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <HomeComponent/>
+                        <HomeComponent name={'Harshit'} lastname={'Bhatt'} address={'Jammu'} user={user}/>
                     </div>
                 </div>
             </div>
-
         );
     }
 }
