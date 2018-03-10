0a5dc3ca8cbea2ed9cfa81e58add39327fedc6fa
Your Name <harshit.hartron@gmail.com>
2018-03-10 13:58:16 +0530

    added header and home components

---
+++ src/app/components/Home.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class HomeComponent extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>In a New COmponent!!</p>
+            </div>
+        );
+    }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,32 @@
-console.log('It works fine');
+import React from "react";
+import {render} from "react-dom";
+import { HeaderComponent } from "./components/Header.component";
+import { HomeComponent } from  "./components/Home.component";
+
+
+class App extends React.Component {
+    render() {
+        let component="ghas";
+        if (true){
+            component=<p>Sass</p>;
+        }
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <HeaderComponent/>
+                        { component }
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <HomeComponent/>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById("app"));
